07bc2ccc6b467127790e86625f7de3525991195b
Engel Dark <morenomoralesluisangel@gmail.com>
2023-04-18 19:33:29 -0500

    primer commit

---
+++ src/components/ComNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ComNav extends React.Component{
+    render() {
+        return (
+             <div className='ComNav-component' class="row  fa-border ">
+                <h1>menu</h1>
+             </div>
+        );
+    }
+}
+export default ComNav;
